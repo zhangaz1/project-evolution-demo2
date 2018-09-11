c7296260efc411b68c6033e0648b71eb33097b1d
zhangaz1 <zhangaz1@hotmail.com>
2018-09-11 16:18:56 +0800

    add some test

--- src/client/src/app/core/games/games.component.spec.ts
+++ src/client/src/app/core/games/games.component.spec.ts
@@ -3,22 +3,35 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
+import { Router } from '@angular/router';
+
+import { IGame } from '@@shared/core/interfaces/index';
+import { GamesService } from '../../shared/games/games.service';
+
 import { GamesComponent } from './games.component';
 
 describe('GamesComponent', () => {
   let component: GamesComponent;
+  let gamesService: GamesService;
   let fixture: ComponentFixture<GamesComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ GamesComponent ]
-    })
-    .compileComponents();
+    TestBed
+      .configureTestingModule({
+        imports: [],
+        declarations: [GamesComponent],
+        providers: [
+          Router,
+          GamesService,
+        ],
+      })
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GamesComponent);
     component = fixture.componentInstance;
+    gamesService = TestBed.get(GamesService);
     fixture.detectChanges();
   });
 
--- src/client/src/app/shared/games/games.service.spec.ts
+++ src/client/src/app/shared/games/games.service.spec.ts
@@ -2,15 +2,42 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { GamesService } from './games.service';
+import { Observable } from 'rxjs';
+
+import { IGame } from '@@shared/core/interfaces/index';
 
 describe('Service: Games', () => {
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [GamesService]
     });
   });
 
-  it('should ...', inject([GamesService], (service: GamesService) => {
+  it('should be injected', inject([GamesService], (service: GamesService) => {
     expect(service).toBeTruthy();
   }));
+
+  describe('Methods', () => {
+    let gamesService: GamesService;
+    let games: Observable<IGame[]>;
+
+    beforeEach(() => {
+      gamesService = TestBed.get(GamesService);
+    });
+
+    it('should can get games', () => {
+      games = gamesService.getGames();
+      expect(games).toBeTruthy();
+    });
+
+    describe('games', () => {
+
+      it('should be Observable', () => {
+        const result = games instanceof Observable;
+        expect(result).toBeTruthy();
+      });
+
+    });
+  });
 });
