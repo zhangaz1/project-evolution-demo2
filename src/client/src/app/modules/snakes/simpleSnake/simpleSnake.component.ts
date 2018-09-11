6c60eb06c8821ef8c92bf1de1adfe4f5b030e4f6
zhangaz1 <zhangaz1@hotmail.com>
2018-09-11 14:30:54 +0800

    can run

--- src/client/src/app/modules/snakes/simpleSnake/simpleSnake.component.ts
+++ src/client/src/app/modules/snakes/simpleSnake/simpleSnake.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import SimpleSnake from '@@shared/modules/snakes/simpleSnake/snake';
 
 @Component({
   selector: 'app-simple-snake',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SimpleSnakeComponent implements OnInit {
 
+  @ViewChild('gameGround')
+  private gameGround;
+
+  private game: SimpleSnake;
+
   constructor() { }
 
-  ngOnInit() {
+  public ngOnInit() {
+    this.game = new SimpleSnake(this.gameGround.nativeElement);
   }
 
 }
